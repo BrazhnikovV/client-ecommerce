7d6e61ba2e61877326c9e302ce498c1771ead4a7
BrazhnikovV <brazhnikov_ve@mail.ru>
2019-08-05 12:50:15 +0300

    Декомпозиция верстки шаблона на компоненты.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './layout/app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { CopyrightComponent } from './layout/copyright/copyright.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CopyrightComponent
   ],
   imports: [
     BrowserModule
